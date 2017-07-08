d0a5a890a596ffe6161a43ad6120b0efac82085c
Manuel Micu <manu4216@gmail.com>
2017-07-08 12:28:20 +0200

    Restructure files in folders

---
+++ client/src/Content/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="Content">
+                <h2>Explore nightlife oportunities</h2>
+            </div>
+        );
+    }
+}
+
+export default Content;
